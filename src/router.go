c1c76c9554dddec16e4a91455223d6593bbeccc5
Dudnik Pavel <ne.pridumal.nik@yandex.ru>
2024-03-03 20:35:09 +0700

    Добавлена обработка статических ресурсов

--- src/router.go
+++ src/router.go
@@ -20,6 +20,14 @@ func CreateServer() *Server {
 	server.router = mux.NewRouter()
 	server.router.HandleFunc("/upload", upload).Methods("POST")
 
+	// Добавление скриптов
+	jsHandler := http.StripPrefix("/js/", http.FileServer(http.Dir("./ui/js/")))
+	server.router.PathPrefix("/js").Methods("GET").Handler(jsHandler)
+
+	// Добавление html файлов
+	htmlHandler := http.StripPrefix("/", http.FileServer(http.Dir("./ui/html/")))
+	server.router.PathPrefix("/").Methods("GET").Handler(htmlHandler)
+
 	return &server
 }
 
