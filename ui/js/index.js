03992fdb939f45cdb9733a1cb6cec0fa4e93beb5
Dudnik Pavel <ne.pridumal.nik@yandex.ru>
2024-03-05 09:03:25 +0700

    Добавлены тестовые компоненты

--- ui/js/index.js
+++ ui/js/index.js
@@ -1,1 +1,38 @@
-ReactDOM.render(<h1>Привет!</h1>, document.getElementById("app"))
+const Block = () => {
+    return <h2>Это блок</h2>
+}
+
+class Button extends React.Component {
+    render() {
+        const increment = () => {
+            this.props.onClick()
+        }
+
+
+        return (<button onClick={increment}> Кнопа</button >)
+    }
+}
+
+class App extends React.Component {
+    render() {
+        let counter = 0
+        const click = () => {
+            counter++
+            console.log("counter: " + counter)
+        }
+
+        return (
+            <div>
+                <h1>Я компонент App: {counter}</h1>
+                <Block />
+                <Button onClick={click} />
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App>
+
+    </App>, document.getElementById("app")
+)
