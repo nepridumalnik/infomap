26f7acd40e0e3a93ce422385869797a0ea035f81
Dudnik Pavel <ne.pridumal.nik@yandex.ru>
2024-03-06 22:32:13 +0700

    Добавлено изменение состояний

--- ui/js/index.js
+++ ui/js/index.js
@@ -1,6 +1,5 @@
-const Block = () => {
-    return <h2>Это блок</h2>
-}
+// https://react.dev/learn
+// https://ru.react.js.org/docs/react-component.html
 
 class Button extends React.Component {
     render() {
@@ -8,24 +7,25 @@ class Button extends React.Component {
             this.props.onClick()
         }
 
-
         return (<button onClick={increment}> Кнопа</button >)
     }
 }
 
 class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { counter: 0 }
+    }
+
     render() {
-        let counter = 0
-        const click = () => {
-            counter++
-            console.log("counter: " + counter)
+        const increment = () => {
+            this.setState({ counter: this.state.counter + 1 })
         }
 
         return (
             <div>
-                <h1>Я компонент App: {counter}</h1>
-                <Block />
-                <Button onClick={click} />
+                <div>Было сделано "{this.state.counter}" нажатий</div>
+                <Button onClick={increment} />
             </div>
         )
     }
