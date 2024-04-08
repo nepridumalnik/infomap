b414f98a49fbeefc17e0fdbebf5de07c503b389d
Dudnik Pavel <ne.pridumal.nik@yandex.ru>
2024-04-08 11:20:31 +0700

    Добавлена загрузка файла в браузер

--- ui/js/index.js
+++ ui/js/index.js
@@ -8,3 +8,25 @@ const unauth = () => {
         console.log("Error: " + error)
     })
 }
+
+// Отправка файла
+const uploadFile = () => {
+    // Получаем форму по ID
+    const form = document.getElementById('uploadForm');
+
+    // Создаем объект FormData и добавляем файл из формы
+    const formData = new FormData(form);
+
+    // Отправляем файл на сервер с помощью Axios
+    axios.post('/upload', formData, {
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        }
+    }).then((response) => {
+        // Обрабатываем успешный ответ, если необходимо
+        console.log('Файл успешно загружен', response);
+    }).catch((error) => {
+        // Обрабатываем ошибку, если необходимо
+        console.error('Ошибка при загрузке файла', error);
+    });
+}
